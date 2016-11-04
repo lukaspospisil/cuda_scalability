eb9725396401b989710354de6cce7553e7d4ab23
lukaspospisil <lukas.pospisil@vsb.cz>
2016-11-04 14:03:11 +0100

    sample working on anselm

--- sample.cu
+++ sample.cu
@@ -3,13 +3,13 @@
 
 
 /* the length of testing vector */
-#define T 10
+#define T 1000000
 
 /* the number of levels (number fo subproblems) */
 #define LEVELS 3
 
 /* if the lenght of vector is large, set this to zero */
-#define PRINT_VECTOR_CONTENT 1
+#define PRINT_VECTOR_CONTENT 0
 
 /* which CUDA calls to test? */
 #define CALL_NAIVE 1
@@ -38,7 +38,8 @@ __global__ void mykernel(double *x_arr, int mysize){
 	if(i<mysize){ /* maybe we call more than n_local kernels */
 		x_arr[i] = i;
 	}
-	/* if i >= mysize then relax and do nothing */	
+
+	/* i >= mysize then relax and do nothing */	
 }
 
 /* print kernel, call only one (!)  */
@@ -47,7 +48,7 @@ __global__ void mykernel(double *x_arr, int mysize){
 __global__ void printkernel(double *x_arr, int mysize){
 	printf("  [");
 	for(int i=0;i<mysize;i++){
-		printf(" %d", x_arr[i]);
+		printf(" %f", x_arr[i]);
 		if(i < mysize-1) printf(",");
 	}
 	printf(" ]\n");
@@ -61,6 +62,10 @@ __global__ void printkernel(double *x_arr, int mysize){
 
 int main( int argc, char *argv[] )
 {
+#ifdef USE_CUDA
+	gpuErrchk( cudaDeviceReset() );
+#endif
+
 	/* print problem info */
 	std::cout << "Benchmark started" << std::endl;
 	std::cout << " T          = " << T << std::endl;
@@ -72,7 +77,7 @@ int main( int argc, char *argv[] )
 
 	for(int level=0; level < LEVELS; level++){
 		/* compute the size of testing array on this level */
-		mysize = ceil(T/(level+1));
+		mysize = ceil(T/(double)(level+1));
 		std::cout << "(" << level+1 << ".): problem of size = " << mysize << std::endl;
 
 #ifdef USE_CUDA
@@ -84,7 +89,7 @@ int main( int argc, char *argv[] )
 		/* fill array */
 		if(CALL_NAIVE){
 			/* the easiest call */
-			mykernel<<<mysize, 1>>>(x_arr,mysize); 
+			mykernel<<<1, mysize>>>(x_arr,mysize); 
 			gpuErrchk( cudaDeviceSynchronize() ); /* synchronize threads after computation */
 		}
 
@@ -105,7 +110,7 @@ int main( int argc, char *argv[] )
 		}
 
 		/* destroy array */
-		gpuErrchk( cudaFree(&x_arr) );
+		gpuErrchk( cudaFree(x_arr) );
 
 #else
 /* ------- SEQUENTIAL version ------- */
